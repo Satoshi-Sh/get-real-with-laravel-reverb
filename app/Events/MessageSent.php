4ad9bc2f96995bad3b34eca69f131466745059e8
satoshi-sh <satoss1108@gmail.com>
2025-10-08 09:13:07 -0500

    Finished ep 4

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -4,6 +4,7 @@ namespace App\Events;
 
 use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -21,6 +22,6 @@ class MessageSent implements ShouldQueue, ShouldBroadcast
         $message->load('user');
     }
     public function broadcastOn(): Channel{
-        return new Channel('channels.' . $this->message->channel_id);
+        return new PrivateChannel('channels.' . $this->message->channel_id);
     }
 }
